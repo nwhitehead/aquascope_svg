5172098e148c954321f6f2a3adeba19f39f2b5f7
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-16 12:50:42 -0700

    Add json parse.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
+use serde_json::Value;
+use std::fs;
 
 #[derive(Parser)]
 #[command(name = "aquascope_svg")]
-#[command(about = "A tool for working with aquascope SVGs", long_about = None)]
+#[command(about = "A tool for converting Aquascope JSON to SVG", long_about = None)]
 struct Args {
     #[arg(help = "Input filename")]
     input: String,
@@ -10,5 +12,7 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    println!("{}", args.input);
+    let content = fs::read_to_string(&args.input).expect("Failed to read input file");
+    let json: Value = serde_json::from_str(&content).expect("Failed to parse JSON");
+    println!("{:?}", json);
 }

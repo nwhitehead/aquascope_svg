22a95085897b6e5ed8c00de282aca66509ddbc7d
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-19 14:08:01 -0700

    Use inlining

--- parse/main.py
+++ parse/main.py
@@ -1,6 +1,53 @@
+import argparse
 from lark import Lark
 
-l = Lark(open('grammar.lark').read())
+l = Lark(r"""
+
+%import common.WS_INLINE
+%import common.NEWLINE
+%import common.C_COMMENT
+%import common.CPP_COMMENT
+%import common.SIGNED_NUMBER
+
+// Ignore inline whitespace (but keep newlines)
+%ignore WS_INLINE
+
+// Allow C and C++ style comments
+%ignore C_COMMENT
+%ignore CPP_COMMENT
+
+%import common.CNAME
+
+TEXT: /[^\n]+/
+EOL: NEWLINE+
+UNESCAPED_LABEL: CNAME
+ESCAPED_LABEL: "`" /[^`]+/ "`"
+DIGITS: /[\d]+/
+
+label: UNESCAPED_LABEL | ESCAPED_LABEL
+
+start: [EOL] step*
+
+step: "# " TEXT EOL location*
+
+location: "## " TEXT EOL ( region* | def* )
+
+region: "### " TEXT EOL def*
+
+def: label ":" value EOL
+
+destination: label ("." DIGITS)* borrow
+borrow: "'"*
+
+value: SIGNED_NUMBER
+| "[" value ("," value)* "]" -> array_value
+| "(" value ("," value)* ")" -> tuple_value
+| "'" /[^']/ "'" -> char_value
+| label "{" (label ":" value ("," label ":" value)*)? "}" -> struct_value
+| "ptr" "(" destination ")" -> ptr_value
+| "*" -> invalid_value
+
+""")
 
 def main():
     tree = l.parse("""

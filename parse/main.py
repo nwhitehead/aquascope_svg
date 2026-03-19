0d49d716792aec4c0ba541e5b89ae75c7ff7c8b3
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-19 13:21:56 -0700

    Working on lark grammar.

--- parse/main.py
+++ parse/main.py
@@ -1,9 +1,39 @@
 from lark import Lark
 
-l = Lark(open('test_grammar.lark').read())
+l = Lark(open('grammar.lark').read())
 
 def main():
-    print( l.parse("Hello, World!") )
+    print( l.parse("""
+# L0
+## Stack
+### main
+
+# L1
+## Stack
+### main
+x: ptr(H0)
+## Heap
+H0: 0
+
+# L2
+## Stack
+### main
+x: ptr(H0)
+## Heap
+H0: 1
+
+# L3
+## Stack
+### main
+x: ptr(H0)
+y: ptr(H0)
+## Heap
+H0: 1
+
+# L4
+## Stack
+### main
+""") )
 
 if __name__ == "__main__":
     main()

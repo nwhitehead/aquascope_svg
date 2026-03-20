c3c1d8a67b70251b43bd9a209aa5c72e8b03b450
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-20 10:20:57 -0700

    Working on rendering glue code.

--- src/render.rs
+++ src/render.rs
@@ -8,15 +8,51 @@ pub fn render(prg: &Program) -> Result<String> {
 }
 
 fn render_program(prg: &Program) -> Result<String> {
-    let steps = &prg.0;
-    let pieces: Result<Vec<String>> = steps
-        .into_iter()
-        .map(|s| render_step(&s))
-        .collect();
-    let res = pieces?.join("");
+    let mut res = String::new();
+    for step in &prg.0 {
+        let piece = render_step(&step)?;
+        res.push_str(&piece);
+    }
     Ok(res)
 }
 
 fn render_step(step: &Step) -> Result<String> {
-    Ok("<step>".into())
+    let mut res = String::new();
+    res.push_str(&step.label);
+    res.push_str(" -> ");
+    for location in &step.locations {
+        let piece = render_location(&location)?;
+        res.push_str(&piece);
+    }
+    Ok(res)
+}
+
+fn render_location(loc: &Location) -> Result<String> {
+    let mut res = String::new();
+    res.push_str(&loc.name);
+    res.push_str(" -> ");
+    // A location either has definitions itself (and no regions) OR it has regions and no definitions
+    assert!(loc.definitions.is_empty() || loc.regions.is_empty());
+    if !loc.definitions.is_empty() {
+        let piece = render_definitions(&loc.definitions)?;
+        res.push_str(&piece);
+    } else {
+        for region in &loc.regions {
+            let piece = render_region(&region)?;
+            res.push_str(&piece);
+        }
+    }
+    Ok(res)
+}
+
+fn render_region(region: &Region) -> Result<String> {
+    let mut res = String::new();
+    res.push_str(&region.name);
+    let pieces = render_definitions(&region.definitions)?;
+    res.push_str(&pieces);
+    Ok(res)
+}
+
+fn render_definitions(definitions: &[Def]) -> Result<String> {
+    Ok("[defns]".into())
 }

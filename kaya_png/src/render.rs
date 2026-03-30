6a788f38c981136a4bf7c20d42611b361dc17e27
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-29 20:50:31 -0700

    adding tiny-skia

--- kaya_png/src/render.rs
+++ kaya_png/src/render.rs
@@ -2,6 +2,7 @@ use anyhow::{bail, Result};
 use ab_glyph::{point, Font, Glyph, Point, Rect, ScaleFont};
 use ab_glyph::{FontRef, FontVec, PxScale};
 use image::{DynamicImage, ImageBuffer, Rgb, Rgba};
+use tiny_skia::*;
 
 const TEXT: &str = "This is ab_glyph rendered into a png!";
 
@@ -142,5 +143,30 @@ pub fn test(filename: &str) -> Result<()> {
     canvas.save(filename)?;
 
     println!("Size of \"{}\" is {:?}", txt, canvas.measure_text(txt, 48.0, 9999.0)?);
+
+    let mut paint = Paint::default();
+    paint.set_color_rgba8(0, 127, 0, 200);
+    paint.anti_alias = true;
+
+    let path = {
+        let mut pb = PathBuilder::new();
+        const RADIUS: f32 = 250.0;
+        const CENTER: f32 = 250.0;
+        pb.move_to(CENTER + RADIUS, CENTER);
+        for i in 1..8 {
+            let a = 2.6927937 * i as f32;
+            pb.line_to(CENTER + RADIUS * a.cos(), CENTER + RADIUS * a.sin());
+        }
+        pb.finish().unwrap()
+    };
+
+    let mut stroke = Stroke::default();
+    stroke.width = 6.0;
+    stroke.line_cap = LineCap::Round;
+    stroke.dash = StrokeDash::new(vec![20.0, 40.0], 0.0);
+
+    let mut pixmap = Pixmap::new(500, 500).unwrap();
+    pixmap.stroke_path(&path, &paint, &stroke, Transform::identity(), None);
+    pixmap.save_png("image.png").unwrap();
     Ok(())
 }

d061b9c20f89fbc3cd1f3d2fdda922ec676a78be
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-31 13:12:22 -0700

    unit tests for color function

--- kaya_png/src/render.rs
+++ kaya_png/src/render.rs
@@ -25,6 +25,7 @@ pub fn render_value(value: &Value, render_state: &mut RenderState) -> Box<dyn Dr
         Value::Number(v) => {
             ds.font = style.get_string_or("value.number.font", mono);
             ds.text_color = style.get_color_or("value.number.color", black);
+            ds.font_size = style.get_number_or("value.number.font_size", 24.0);
             let text = format!("{}", v);
             return Box::new(GText::new(&text, point(0.0, 0.0), ds));
         },
@@ -56,9 +57,63 @@ pub fn render_value(value: &Value, render_state: &mut RenderState) -> Box<dyn Dr
     panic!("unreachable");
 }
 
+fn color(txt: &str) -> Result<ColorU8> {
+    let mut r = 255;
+    let mut g = 255;
+    let mut b = 255;
+    let mut a = 255;
+    if !txt.starts_with('#') {
+        bail!("colors must start with #");
+    }
+    let txt = txt.trim_start_matches('#');
+    match txt.len() {
+        3 => {
+            r = u8::from_str_radix(&txt[0..1], 16)? * (0x10 + 0x01);
+            g = u8::from_str_radix(&txt[1..2], 16)? * (0x10 + 0x01);
+            b = u8::from_str_radix(&txt[2..3], 16)? * (0x10 + 0x01);
+        },
+        4 => {
+            r = u8::from_str_radix(&txt[0..1], 16)? * (0x10 + 0x01);
+            g = u8::from_str_radix(&txt[1..2], 16)? * (0x10 + 0x01);
+            b = u8::from_str_radix(&txt[2..3], 16)? * (0x10 + 0x01);
+            a = u8::from_str_radix(&txt[3..4], 16)? * (0x10 + 0x01);
+        },
+        6 => {
+            r = u8::from_str_radix(&txt[0..2], 16)?;
+            g = u8::from_str_radix(&txt[2..4], 16)?;
+            b = u8::from_str_radix(&txt[4..6], 16)?;
+        },
+        8 => {
+            r = u8::from_str_radix(&txt[0..2], 16)?;
+            g = u8::from_str_radix(&txt[2..4], 16)?;
+            b = u8::from_str_radix(&txt[4..6], 16)?;
+            a = u8::from_str_radix(&txt[6..8], 16)?;
+        },
+        _ => bail!("unknown color length"),
+    }
+    Ok(ColorU8::from_rgba(r, g, b, a))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    pub fn test_color() -> Result<()> {
+        assert_eq!(color("#000")?, ColorU8::from_rgba(0, 0, 0, 0xff));
+        assert_eq!(color("#f00")?, ColorU8::from_rgba(0xff, 0, 0, 0xff));
+        assert_eq!(color("#080")?, ColorU8::from_rgba(0, 0x88, 0, 0xff));
+        assert_eq!(color("#00a")?, ColorU8::from_rgba(0, 0, 0xaa, 0xff));
+        assert_eq!(color("#0008")?, ColorU8::from_rgba(0, 0, 0, 0x88));
+        assert_eq!(color("#1234")?, ColorU8::from_rgba(0x11, 0x22, 0x33, 0x44));
+        assert_eq!(color("#123456")?, ColorU8::from_rgba(0x12, 0x34, 0x56, 0xff));
+        assert_eq!(color("#12345678")?, ColorU8::from_rgba(0x12, 0x34, 0x56, 0x78));
+        assert!(color("000").is_err());
+        assert!(color("#12345").is_err());
+        assert!(color("#0g3456").is_err());
+        assert!(color("#fffffffff").is_err());
+        Ok(())
+    }
 
     #[test]
     pub fn test_render_value() -> Result<()> {
@@ -72,6 +127,8 @@ mod tests {
 
         let mut rs = RenderState::default();
         rs.style.add_string("value.number.font", "mono");
+        rs.style.add_number("value.number.font_size", 48.0);
+        rs.style.add_color("value.number.color", color("#f00")?);
         let mut v = render_value(&Value::Number(42.0), &mut rs);
         v.translate(point(400.0, 400.0));
         v.draw(&mut canvas)?;

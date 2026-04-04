6b7e24a5a02e5c41af954f158c0c48a2f4c1c93b
Nathan Whitehead <nwhitehe@gmail.com>
2026-04-03 20:33:01 -0700

    thicker arrow

--- kaya_png/src/arrow.rs
+++ kaya_png/src/arrow.rs
@@ -1,22 +1,26 @@
 #![allow(unused)]
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 use ab_glyph::{Point, Rect, point};
+use tiny_skia::{Paint, PathBuilder, Transform};
 
 use crate::canvas::Canvas;
 use crate::draw::Drawable;
 use crate::draw_state::DrawState;
+use crate::style::color;
 
 #[derive(Clone, Debug)]
 pub struct Arrow {
     start: Point,
+    start_control: Point,
     end: Point,
+    end_control: Point,
     state: DrawState,
 }
 
 impl Arrow {
-    pub fn new(start: Point, end: Point, state: DrawState) -> Self {
-        Self { start, end, state }
+    pub fn new(start: Point, start_control: Point, end: Point, end_control: Point, state: DrawState) -> Self {
+        Self { start, start_control, end, end_control, state }
     }
 }
 
@@ -32,6 +36,26 @@ impl Drawable for Arrow {
         })
     }
     fn draw(&self, canvas: &mut Canvas) -> Result<()> {
+        let color = self.state.stroke_color;
+        let mut paint = Paint::default();
+        paint.set_color_rgba8(color.red(), color.green(), color.blue(), color.alpha());
+        paint.anti_alias = true;
+        let Some(path) = ({
+            let mut pb = PathBuilder::new();
+            pb.move_to(self.start.x, self.start.y);
+            pb.line_to(self.end.x, self.end.y);
+            pb.finish()
+        }) else {
+            bail!("could not make path");
+        };
+        canvas.pixmap.stroke_path(
+            &path,
+            &paint,
+            &self.state.stroke,
+            Transform::identity(),
+            None,
+        );
+
         Ok(())
     }
     fn clone_box(&self) -> Box<dyn Drawable> {
@@ -39,5 +63,43 @@ impl Drawable for Arrow {
     }
     fn get_tagged(&self, _id: &str) -> Option<Box<dyn Drawable>> {
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::style::standard_style;
+    use crate::draw::GBox;
+    use tiny_skia::{Color, ColorU8};
+
+    #[test]
+    pub fn test_draw_arrow() -> Result<()> {
+        let mut canvas = Canvas::new(800, 800)?;
+        canvas
+            .pixmap
+            .fill(Color::from_rgba(0.2, 0.1, 0.3, 1.0).unwrap());
+
+        canvas.load_font(
+            "mono",
+            include_bytes!("../fonts/DejaVu/DejaVuSansMono-Bold.ttf"),
+        )?;
+        canvas.load_font("serif", include_bytes!("../fonts/Lato/Lato-Regular.ttf"))?;
+
+        let mut ds = DrawState::default();
+        ds.stroke.width = 20.0;
+        ds.stroke_color = color("#ff0")?;
+    
+        let arrow = Arrow::new(
+            point(100.0, 100.0),
+            point(150.0, 100.0),
+            point(300.0, 200.0),
+            point(250.0, 200.0),
+            ds,
+        );
+        arrow.draw(&mut canvas)?;
+
+        canvas.save("test_render_arrow.png")?;
+        Ok(())
     }
 }

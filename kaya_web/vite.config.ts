62980c0d5659607f555e07c84401cedba8c53d1a
Nathan Whitehead <nwhitehe@gmail.com>
2026-04-09 20:59:09 -0700

    fixup vite serving

--- kaya_web/vite.config.ts
+++ kaya_web/vite.config.ts
@@ -5,31 +5,37 @@ import Unocss from 'unocss/vite';
 import Components from 'unplugin-vue-components/vite';
 import VueRouter from 'unplugin-vue-router/vite';
 
-import { defineConfig } from 'vite'
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+    resolve: {
+        alias: {
+            '~/': `${path.resolve(__dirname, 'src')}/`,
+        },
     },
-  },
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/element/index.scss" as *;`,
-        api: 'modern-compiler',
-      },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "~/styles/element/index.scss" as *;`,
+                api: 'modern-compiler',
+            },
+        },
     },
-  },
-
-  plugins: [
-    Vue(),
-    Components({
-      extensions: ['vue'],
-      include: [/\.vue$/, /\.vue\?vue/],
-      dts: 'src/components.d.ts',
-    }),
-    Unocss(),
-  ],
+    plugins: [
+        Vue(),
+        Components({
+            extensions: ['vue'],
+            include: [/\.vue$/, /\.vue\?vue/],
+            dts: 'src/components.d.ts',
+        }),
+        Unocss(),
+    ],
+    server: {
+        fs: {
+            allow: [
+                searchForWorkspaceRoot(process.cwd()),
+                '../kaya_ts/pkg',
+            ],
+        },
+    },
 });
